08ff4c24bb839e01beae81558d79cbd892e157d2
Ayush Goel <76999903+Ayushhg@users.noreply.github.com>
2025-03-26 07:03:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Early Six Analyis by Ayush Goel")
 
 df_ballByball = pd.read_csv("ball_data.csv")
 df_match = pd.read_csv("match_data.csv")
-
+df_Ball_Batter = pd.read_csv("ball_data.csv")
 
 batting_team = st.selectbox("Choose Batting Team",df_ballByball["BattingTeam"].unique())
 
@@ -38,4 +38,21 @@ opponent_counts.columns = ["Opponent Team", "Number of Matches with Early Six"]
 
 st.write("Early Sixes Breakdown by Opponent Team:")
 st.dataframe(opponent_counts)
+
+player_selected = st.selectbox("Choose Batting Team",df_Ball_Batter["Batter"].unique())
+
+df_Ball_Batter = df_Ball_Batter[df_Ball_Batter["Batter"] == player_selected]
+
+num_unique_ids = df_Ball_Batter["ID"].nunique()
+st.write("Total Number of Matches :",num_unique_ids)
+
+df_grouped = df_Ball_Batter.groupby(["ID", "Overs"], as_index=False).agg({"TotalRun": "max"})
+
+df_filtered = df_grouped[df_grouped["Overs"].isin([0, 1])]
+
+df_six_runs = df_filtered[df_filtered["TotalRun"] >= 6]
+
+num_matches_with_early_six = df_six_runs["ID"].nunique()
+
+st.write("Number of Matches with Early Six:", num_matches_with_early_six)
 
